47f8c6318e025cd919b24b18b54964271f010f2e
Simon Diesenreiter <disi33@gmail.com>
2020-03-12 03:50:40 -0700

    fix version replacement logic

--- ci_scripts/update_helm_chart.sh
+++ ci_scripts/update_helm_chart.sh
@@ -4,13 +4,14 @@ HELM_CHART="website"
 
 VERSION_NUMBER=$1
 
-git clone https://github.com/disi33/personal_website_helm_charts /tmp/helm_charts &> /dev/null
+git clone https://github.com/disi33/personal_website_helm_charts /tmp/helm_charts
 pushd /tmp/helm_charts/$HELM_CHART/
-sed -i 's/tag: .*/tag: $VERSION_NUMBER/g' /tmp/helm_charts/$HELM_CHART/values-imgtag.yaml
-sed -i 's/version: .*/version: $VERSION_NUMBER/g' /tmp/helm_charts/$HELM_CHART/Chart.yaml
+sed -i 's/[0-9]\+\.[0-9]\+\.[0-9]\+/'"$VERSION_NUMBER"'/g' /tmp/helm_charts/$HELM_CHART/values-imgtag.yaml
+sed -i 's/[0-9]\+\.[0-9]\+\.[0-9]\+/'"$VERSION_NUMBER"'/g' /tmp/helm_charts/$HELM_CHART/Chart.yaml
 git add /tmp/helm_charts/$HELM_CHART/values-imgtag.yaml
 git add /tmp/helm_charts/$HELM_CHART/Chart.yaml
 git commit -m "[auto] bump $HELM_CHART chart version number to $VERSION_NUMBER"
+git config remote.origin.url https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/disi33/personal_website_helm_charts
 git push origin master
 popd
 
