f43b87077b8b78505a556cd7856eab55c24c7f06
Simon Diesenreiter <disi33@gmail.com>
2020-03-14 00:10:55 -0700

    add ingress and handle port forwarding

--- Install-HelmCharts.ps1
+++ Install-HelmCharts.ps1
@@ -1,9 +1,31 @@
 $directoryNames = Get-ChildItem -Path . -Directory -Force -ErrorAction SilentlyContinue | Select-Object Name
 $helmCharts = $directoryNames | Where-Object -Property Name -NotLike ".*"
+$extraCharts = @("helm")
+
+. helm repo add stable https://kubernetes-charts.storage.googleapis.com
+. helm repo update
+netsh interface portproxy add v4tov4 listenaddress=192.168.1.118 listenport=8443 connectaddress=192.168.1.123 connectport=8443
+netsh interface portproxy add v4tov4 listenaddress=192.168.1.118 listenport=80 connectaddress=192.168.1.123 connectport=80
+netsh interface portproxy add v4tov4 listenaddress=127.0.0.1 listenport=80 connectaddress=192.168.1.123 connectport=80
+#netsh interface portproxy add v4tov4 listenaddress=diesenreiter.com listenport=80 connectaddress=192.168.1.123 connectport=80
+#netsh interface portproxy add v4tov4 listenport=8443 connectaddress=127.0.0.1 connectport=8443 listenaddress=diesenreiter.com protocol=tcp
+#netsh interface portproxy add v4tov4 listenport=80 connectaddress=127.0.0.1 connectport=80 listenaddress=diesenreiter.com protocol=tcp
+
 
 foreach($chart in $helmCharts)
 {
     $chartname = $chart.Name
 
     helm upgrade --install $chartname ./$chartname --values ./$chartname/values.yaml --values ./$chartname/values-imgtag.yaml
+}
+
+foreach($extraChart in $extraCharts)
+{
+    switch($extraChart)
+    {
+        "helm" 
+        {
+            minikube addons enable ingress
+        }
+    }
 }
