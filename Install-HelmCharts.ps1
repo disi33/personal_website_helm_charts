98f3c9864921ae489fb7ebe1882af8d868c8ed74
Simon Diesenreiter <disi33@gmail.com>
2020-03-30 02:51:26 -0700

    add minor stuff to install scripts

--- Install-HelmCharts.ps1
+++ Install-HelmCharts.ps1
@@ -1,3 +1,5 @@
+# minikube start --apiserver-names=diesenreiter.com
+
 $directoryNames = Get-ChildItem -Path . -Directory -Force -ErrorAction SilentlyContinue | Select-Object Name
 $helmCharts = $directoryNames | Where-Object -Property Name -NotLike ".*"
 $extraCharts = @("helm")
@@ -7,9 +9,11 @@ $extraCharts = @("helm")
 netsh interface portproxy add v4tov4 listenaddress=192.168.1.118 listenport=8443 connectaddress=192.168.1.123 connectport=8443
 netsh interface portproxy add v4tov4 listenaddress=192.168.1.118 listenport=80 connectaddress=192.168.1.123 connectport=80
 netsh interface portproxy add v4tov4 listenaddress=127.0.0.1 listenport=80 connectaddress=192.168.1.123 connectport=80
+netsh interface portproxy add v4tov4 listenaddress=127.0.0.1 listenport=8443 connectaddress=192.168.1.123 connectport=8443
 #netsh interface portproxy add v4tov4 listenaddress=diesenreiter.com listenport=80 connectaddress=192.168.1.123 connectport=80
-#netsh interface portproxy add v4tov4 listenport=8443 connectaddress=127.0.0.1 connectport=8443 listenaddress=diesenreiter.com protocol=tcp
-#netsh interface portproxy add v4tov4 listenport=80 connectaddress=127.0.0.1 connectport=80 listenaddress=diesenreiter.com protocol=tcp
+#netsh interface portproxy add v4tov4 listenaddress=diesenreiter.com listenport=8443 connectaddress=192.168.1.123 connectport=8443
+#netsh interface portproxy add v4tov4 listenaddress=diesenreiter.com listenport=8443 connectaddress=127.0.0.1 connectport=8443 protocol=tcp
+#netsh interface portproxy add v4tov4 listenaddress=diesenreiter.com listenport=80 connectaddress=127.0.0.1 connectport=80 protocol=tcp
 
 git checkout master
 git pull origin master --rebase
